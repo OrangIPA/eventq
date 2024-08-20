f0cdbd81b23b0e545c778605570eb6685a6c65a1
Bahana Alana Khansa <alanabahana102@gmail.com>
2024-08-21 01:00:17 +0700

    login ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  css: ['assets/css/tailwind.css'],
+  modules: ["@nuxt/ui"],
   colorMode: {
     classSuffix: "",
   },
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="flex justify-center items-center min-h-screen bg-zinc-950">
+  <div class="flex min-h-screen items-center justify-center bg-zinc-950">
+    <div class="flex flex-col items-center gap-3">
+      <h1 class="mb-1 text-xl">Login</h1>
+      <UFormGroup label="Email">
+        <UInput :ui="{ color: '' }" type="email" />
+      </UFormGroup>
+      <UFormGroup label="Password">
+        <UInput :ui="{ color: '' }" type="password" />
+      </UFormGroup>
+      <UButton color="sky" class="mt-1">Submit</UButton>
+    </div>
   </div>
 </template>
 
